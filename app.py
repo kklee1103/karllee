5af71418ce1518e918a9c2c1f7f846f01dbcede6
Karl Lee <karllee@Karls-MacBook-Pro.local>
2025-07-17 16:21:09 -0700

    first commit

---
+++ app.py
@@ -0,0 +1,19 @@
+# import the Flask class from the flask module
+from flask import Flask, render_template
+
+# create the application object
+app = Flask(__name__)
+
+# use decorators to link the function to a url
+@app.route('/')
+def home():
+    return "Hello, World!"  # return a string
+
+@app.route('/index')
+def welcome():
+    return render_template('index.html')  # render a template
+
+
+# start the server with the 'run()' method
+if __name__ == '__main__':
+    app.run(debug=True)
